1521759a492ed50a8ad544608bd8ecad3a3277e4
Renesis Group <40821976+renesisgroup@users.noreply.github.com>
2018-07-27 13:57:23 +0600

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -33,6 +33,7 @@ namespace Checkpoints
         ( 3808,    uint256("0x6b1ee9ce991c3727521ecbdde0eaf962f2a57d0666fa0d6910b29ff768351321") ) // next check
         ( 5463,    uint256("0x2317dd1b4b86c9b6da01c4c65e64aba6ee6185ddfb881d93d0cd78d249c71f7b") ) // check after reward doubling
         ( 7412,    uint256("0x9c1b39c7bc7f2b8b02eff70b5a0b07dc933c2bf27fad2c62a20aa52d23e589c1") ) // next check
+        ( 8750,    uint256("0x7315f79f74665bfb70794ee0b74ff67d17b8d1e7c5ffd05d4546b923627a4a60") ) // next check
         ;
 
     // TestNet checkpoints
